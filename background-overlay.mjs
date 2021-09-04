46aa39f70ae1fda26863190d303ca94de277663e
Carlos Fernandez <cfern1990@gmail.com>
2021-09-03 20:59:18 -0400

    Scenes are now configurable

--- background-overlay.mjs
+++ background-overlay.mjs
@@ -2,15 +2,28 @@ function makePositionKey(position) {
     return `${position[0]}_${position[1]}`;
 }
 
+const MODULE = "world-explorer";
+
 export class BackgroundOverlay {
+    initialized = false;
+
     constructor(scene) {
         this.scene = scene;
         this.color = 0xCCCCCC;
-        this.state = { clearing: false };
+        this.state = {};
+
+        const flags = scene.data.flags[MODULE];
+        this._enabled = flags.enabled;
+        this.color = flags.color || 0;
+        this.initialize();
+
+        // this will start loading the image, so do it last
+        this.image = flags.image;
+    }
+
+    initialize() {
         const dimensions = canvas.dimensions;
 
-        this._enabled = scene.getFlag("world-explorer", "enabled"); // pull from scene
-        
         // Draw and add the overlay immediately first.
         // Attempt to not let players see anything until after it loads
         this.overlay = new PIXI.Graphics();
@@ -20,36 +33,51 @@ export class BackgroundOverlay {
             canvas.background.addChild(this.overlay);
         }
 
-        // Create mask (to punch holes in)
+        // Create mask (to punch holes in to reveal tiles/players)
         this.maskTexture = PIXI.RenderTexture.create({
             width: dimensions.width,
             height: dimensions.height,
         });
         this.mask = PIXI.Sprite.from(this.maskTexture);
 
+        // Create mask to put players on
         this.fogSprite = new PIXI.Sprite();
         this.fogSprite.position.set(dimensions.sceneRect.x, dimensions.sceneRect.y);
         this.fogSprite.width = dimensions.sceneRect.width;
         this.fogSprite.height = dimensions.sceneRect.height;
-
-        // hardcoded for now, will need to make it customizable
-        loadTexture("worlds/aftaras/scenes/world-drawn.png").then((texture) => {
-            this.fogSprite.texture = texture;
-        })
-
         this.overlay.addChild(this.fogSprite);
     }
 
     /** Canvas ready again. Anything we added to the background must be re-added */
     ready() {
         if (this.enabled) {
-            canvas.background.addChild(this.overlay); 
+            //canvas.background.addChild(this.overlay); 
             this.overlay.addChild(this.mask);
             this.overlay.mask = this.mask;
+            this._resetState();
             this.refreshMask();
         }
         canvas.grid.addHighlightLayer("exploration");
         this._registerMouseListeners();
+    }
+
+    update(scene) {
+        this.scene = scene;
+        const { enabled, color, image } = scene.data.flags[MODULE];
+        const diff = enabled !== this.enabled || color !== this.color || image !== this.image;
+        this.color = color;
+        this.image = image;
+
+        // Setting enabled state will trigger re-renders if necessary
+        if (diff) {
+            this.enabled = enabled;
+        } else {
+            this.refreshMask();
+        }
+    }
+    
+    get alpha() {
+        return game.user.isGM ? 0.7 : 1;
     }
 
     get enabled() {
@@ -62,15 +90,42 @@ export class BackgroundOverlay {
         if (value) {
             canvas.background.addChild(this.overlay);
             this.refreshOverlay();
-            this.scene.setFlag("world-explorer", "enabled", true);
+            this.refreshMask();
         } else {
             canvas.background.removeChild(this.overlay);
             this.overlay.clear();
+            this._resetState();
         }
     }
 
-    get alpha() {
-        return game.user.isGM ? 0.7 : 1;
+    /** Returns true if the user is currently editing, false otherwise. */
+    get editing() {
+        return this.enabled && this.state.clearing;
+    }
+
+    set editing(value) {
+        if (!this.enabled) return;
+        this.state.clearing = value;
+        canvas.grid.clearHighlightLayer("exploration");
+    }
+
+    set image(image) {
+        this._image = image;
+        this.refreshImage();
+    }
+
+    get image() {
+        return this._image;
+    }
+
+    refreshImage() {
+        if (this.enabled && this.image) {
+            loadTexture(this.image).then((texture) => {
+                this.fogSprite.texture = texture;
+            });
+        } else {
+            this.fogSprite.texture = null;
+        }
     }
 
     refreshOverlay() {
@@ -91,7 +146,7 @@ export class BackgroundOverlay {
         const circleSize = dimensions.size * Math.SQRT2 / 2;
 
         // draw black over the tiles that are revealed
-        for (const position of this.scene.getFlag("world-explorer", "revealed") ?? []) {
+        for (const position of this.scene.getFlag(MODULE, "revealed") ?? []) {
             const [x, y] = canvas.grid.getTopLeft(...position);
             if (canvas.grid.isHex) {
                 const poly = canvas.grid.grid.getPolygon(x, y);
@@ -114,7 +169,7 @@ export class BackgroundOverlay {
 
     isRevealed(x, y) {
         const position = canvas.grid.getCenter(x, y).map(Math.round);
-        const existing = this.scene.getFlag("world-explorer", "revealed");
+        const existing = this.scene.getFlag(MODULE, "revealed");
         const key = makePositionKey(position);
         return existing.some((existing) => makePositionKey(existing) === key);
     }
@@ -124,8 +179,8 @@ export class BackgroundOverlay {
 
         const position = canvas.grid.getCenter(x, y).map(Math.round);
         if (!this.isRevealed(...position)) {
-            const existing = this.scene.getFlag("world-explorer", "revealed");
-            this.scene.setFlag("world-explorer", "revealed", [...existing, position]);
+            const existing = this.scene.getFlag(MODULE, "revealed");
+            this.scene.setFlag(MODULE, "revealed", [...existing, position]);
             return true;
         }
         
@@ -135,22 +190,27 @@ export class BackgroundOverlay {
     unreveal(x, y) {
         if (!this.enabled) return;
         const position = canvas.grid.getCenter(x, y).map(Math.round);
-        const existing = this.scene.getFlag("world-explorer", "revealed") ?? [];
+        const existing = this.scene.getFlag(MODULE, "revealed") ?? [];
         const idx = existing.findIndex((existing) => existing[0] === position[0] && existing[1] === position[1]);
         if (idx >= 0) {
             existing.splice(idx, 1);
-            this.scene.setFlag("world-explorer", "revealed", [...existing]);
+            this.scene.setFlag(MODULE, "revealed", [...existing]);
             return true;
         }
 
         return false;
     }
 
+    _resetState() {
+        this.state = {};
+        this.editing = false;
+    }
+
     _registerMouseListeners() {
         canvas.stage.addListener('pointerdown', (event) => {
             if (!this.enabled) return;
             
-            if (this.state.clearing && event.data.button === 0) {
+            if (this.editing && event.data.button === 0) {
                 const rawPosition = event.data.getLocalPosition(canvas.app.stage);
                 if (!this.reveal(rawPosition.x, rawPosition.y)) {
                     this.unreveal(rawPosition.x, rawPosition.y);
@@ -161,7 +221,7 @@ export class BackgroundOverlay {
         canvas.stage.addListener('pointermove', (event) => {
             if (!this.enabled) return;
 
-            if (this.state.clearing) {
+            if (this.editing) {
                 // Get mouse position translated to canvas coords
                 const position = event.data.getLocalPosition(canvas.app.stage);
                 const [x, y] = canvas.grid.getTopLeft(position.x, position.y);
--- index.js
+++ index.js
@@ -1,19 +1,31 @@
 import { BackgroundOverlay } from "./background-overlay.mjs"
 
+Hooks.on("init", async () => {
+    const defaultSceneConfigRender = SceneConfig.prototype._renderInner;
+    SceneConfig.prototype._renderInner = async function(...args) {
+        const $html = await defaultSceneConfigRender.apply(this, args);
+        const settings = this.entity.data.flags["world-explorer"];
+        const templateName = "modules/world-explorer/templates/scene-settings.html";
+        const template = await renderTemplate(templateName, settings);
+        $html.find("button[type='submit']").before(template);
+        return $html;
+    };
+})
+
 Hooks.on("canvasInit", () => {
     canvas.background.explorationOverlay = new BackgroundOverlay(canvas.scene);
 });
 
 Hooks.on("canvasReady", () => {
-    canvas.background.explorationOverlay.ready();
+    canvas.background.explorationOverlay?.ready();
 });
 
 Hooks.on("createToken", (token) => {
-    if (token.object.observer) canvas.background.explorationOverlay?.refreshMask();
+    if (token.object?.observer) canvas.background.explorationOverlay?.refreshMask();
 });
 
 Hooks.on("updateToken", (token, data) => {
-    if (!token.object.observer) return;
+    if (!token.object?.observer) return;
     if (data.x || data.y) {
         canvas.background.explorationOverlay?.refreshMask();
     }
@@ -24,7 +36,8 @@ Hooks.on("deleteToken", () => {
 });
 
 Hooks.on("updateScene", (scene, data) => {
+    if (scene.id !== canvas.scene.id) return;
     if (data.flags && "world-explorer" in data.flags) {
-        canvas.background.explorationOverlay?.refreshMask();
+        canvas.background.explorationOverlay?.update(scene);
     }
-})
+});
